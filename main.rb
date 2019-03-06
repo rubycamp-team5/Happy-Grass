be05c8b3c603d031e221b3fddfe3808bbc817785
Uekin <36951565+Uekin@users.noreply.github.com>
2019-03-06 12:55:46 +0900

    latest master

--- main.rb
+++ main.rb
@@ -4,12 +4,14 @@ require 'chipmunk'
 require_relative 'scene'
 require_relative 'scenes/opening/director'
 require_relative 'scenes/ending/director'
+require_relative 'scenes/game/director'
 
 Window.width=1024
 Window.height=768
 
 Scene.add(Opening::Director.new,:opening)
 Scene.add(Ending::Director.new,:ending)
+Scene.add(Game::Director.new,:game)
 
 Scene.move_to :opening
 
