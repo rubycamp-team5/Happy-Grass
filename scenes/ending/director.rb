830da1ea57d112b79e1dea5af166340272186420
Kaito Kusaba <ca.ssava26@gmail.com>
2019-03-06 11:28:45 +0900

    Create director.rb

---
+++ scenes/ending/director.rb
@@ -0,0 +1,16 @@
+module Ending
+  class Director
+    def initialize
+      #@bg_img = Image.load('ending_bg.png')
+      @bg_img = Image.new(100, 100, C_WHITE)
+    end
+
+    def play
+      Window.draw(0, 0, @bg_img)
+      scene_transition
+    end
+
+    def scene_transition
+      Scene.move_to(:opening) if Input.key_push?(K_SPACE)
+  end
+end
