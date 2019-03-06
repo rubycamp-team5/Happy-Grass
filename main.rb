f8805f2514ee284e1a67d9e57373cf9cc7d4e342
Uekin <36951565+Uekin@users.noreply.github.com>
2019-03-06 11:13:49 +0900

    feat main.eb,scene.rb

--- main.rb
+++ main.rb
@@ -1,1 +1,15 @@
 require 'dxruby'
+require_relative 'lib/cp/'
+require_relative 'lib/'
+
+Window.width=1024
+Window.height=768
+
+Scane,add(Openig::Director.new,:opening)
+
+Scene.move_to :opening
+
+Window.loop do
+    break if Input.key_push?(K_ESCAPE)
+    Scene.play
+end
