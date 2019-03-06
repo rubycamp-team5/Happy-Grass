e08122e97309adabbfbe671cd1db5508076b3980
Daichi <jouhou1637@gmail.com>
2019-03-06 11:52:49 +0900

    fix mouseSloop

--- lib/cp/cp_string.rb
+++ lib/cp/cp_string.rb
@@ -1,13 +1,15 @@
-class CPCircle < CPBase
+class CPString < CPBase
+  COLLISION_TYPE = 10
   attr_accessor :body, :shape
 
   def initialize(x, y, r, mass, color, image = nil, e = 0.8, u = 0.8)
-    @body = nil
     moment = CP::moment_for_circle(mass, 0, r, CP::Vec2.new(0, 0))
     @body = CP::Body.new(mass, moment)
     @body.p = CP::Vec2.new(x + r, y + r)
     @shape = CP::Shape::Circle.new(@body, r, CP::Vec2.new(0, 0))
+    @shape.parent_obj = self
     @image = image || Image.new(r * 2, r * 2).circle_fill(r, r, r,color)
+    @shape.collision_type = self.class::COLLISION_TYPE
     @r = r
     @x, @y = x, y
     shape.e = e
@@ -18,6 +20,8 @@ class CPCircle < CPBase
   end
 
   def draw
-    Window.draw(@x - @r, @y - @r, @image)
+    apply_force(@x-@body.p.x, @y-@body.p.y)
+    #@body.p = CP::Vec2.new(@x, @y)
+    Window.draw_rot(@body.p.x - @r, @body.p.y - @r, @image, @body.a * 180.0 / Math::PI)
   end
 end
--- src/mouseSloop.rb
+++ src/mouseSloop.rb
@@ -1,49 +1,57 @@
 require "dxruby"
 require "chipmunk"
-require_relative "../lib/cp/"
-line=[]
+require_relative "lib/lib/cp/"
+
+#line=[,]
 
 space=CP::Space.new
-space.gravity=CP::Vec2.new(0,100)
+space.gravity=CP::Vec2.new(0,100/2)
 body = CP::Body.new(1,CP::INFINITY)
 body.p=CP::Vec2.new(100,100)
 shape=CP::Shape::Circle.new(body,40,CP::Vec2.new(0,0))
 space.add_body(body)
 space.add_shape(shape)
-image = Image.load("image/ball-g.png",20,20)
-obj=[]
+image = Image.load("img/ball-g.png",20,20)
 
-wall=CPStaticSlope.new(80,50,200,150,down=100,color=C_BLUE)
-wall2=CPStaticSlope.new(280,250,400,350,-50,color=C_GREEN)
-space.add(wall)
-space.add(wall2)
+obj=[]
+count = 0
+dragSpace = 2
+
+x=Input.mouse_pos_x
+y=Input.mouse_pos_y
+current_Point = [x, y]
 
 Window.loop do
+	p Window.real_fps
+	mem_Point = [x, y]
+
+	x=Input.mouse_pos_x
+        y=Input.mouse_pos_y
+
         if Input.mouse_down?(M_LBUTTON)
-                x=Input.mouse_pos_x
-                y=Input.mouse_pos_y
-                line << [x,y]
-                if line.size()-2>0
-                        l=line.size()-1
-                        pos1=line[l]
-                        pos2=line[l-1]
-                        x1 = [pos1[0],pos2[0]].min
-                        x2 = [pos1[0],pos2[0]].max
-                        y1 = [pos1[1],pos2[1]].min
-                        y2 = [pos1[1],pos2[1]].max
-                        bl=CPStaticBox.new(x1,y1,x2,y2,color=C_RED)
-                        space.add(bl)
-                        obj << bl
-                end
-        else
-                line.clear
-        end
-        
-        for o in obj do
-                        o.draw
-        end
+		current_Point = [x, y]
+		until (mem_Point[0] - current_Point[0]).abs < 2 && (mem_Point[1] - current_Point[1]).abs < 2
+			current_Point[0] = current_Point[0] + ((current_Point[0] > mem_Point[0]) ? -1 : 1)
+			current_Point[1] = current_Point[1] + ((current_Point[1] > mem_Point[1]) ? -1 : 1)
+			count = count + 1
+			if count%5 == 0 then
+				bl = CPCircle.new(current_Point[0], current_Point[1], 5, 10, )
+				obj << bl
+				space.add(bl)
+				count = 0
+			end
+		end
+	end
+
+	
+
+	for o in obj
+		o.draw
+	end
+	
         space.step(1.0/60.0)
-        wall.draw()
-        wall2.draw()
         Window.draw(body.p.x-10, body.p.y+4, image)
+	if body.p.y >= 400
+	   break
+	end
 end
